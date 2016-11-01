95b2ea7255af0ae76753b90d629d1a49ba2517e6
Cal Colson <cpcolson@yahoo.com>
2016-11-01 13:41:25 -0500

    initialize with starting code

---
+++ app/src/main/java/com/example/cal/calshop/ShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.example.cal.calshop;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
